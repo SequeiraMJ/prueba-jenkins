a7f5c58c4d7b84b6c12253a0215d8e325c67074d
SequeiraMJ <mjsequeira@tutanota.com>
2023-09-27 21:07:09 -0600

    Modify ejemplo_jenkins.java to accept parameter from CLI

--- ejemplo_jenkins.java
+++ ejemplo_jenkins.java
@@ -1,20 +1,32 @@
 public class ejemplo_jenkins {
+    /*
+     * En este ejemplo, se declara la clase ejemplo_jenkins que recibe un parámetro
+     * por consola. Es decir, el programa se compila y al momento de ejecutarlo se
+     * le pasa un nombre por parámetro. Por ejemplo: java ejemplo_jenkins Macgyver
+     * luego el parámetro se almacena en la variable name y se ejecutarán las ins-
+     * trucciones dentro del ciclo For.
+     */
     public static void main(String[] args) {
-        /*
-         * Print the message “Hello, I am <NAME> and I am printing the number <X>”
-         * NAME must be a parameter required to run the application
-         * X is the index of the iteration
-         */
-        String name = "Macgyver";
-        for (int i = 0; i < 15; i++) {
-            System.out.println("Hello, I am " + name + " and I am printing the number " + (i + 1));
+        // Verificamos que sólo un parámetro sea pasado al programa, si esto no se
+        // cumple, mostramos en consola un ejemplo de uso e interrumplimos la eje-
+        // cución del programa.
+        if (args.length != 1) {
+            System.err.println("Usage: java ejemplo_jenkins <NAME>");
+            System.exit(1);
+        }
+        // Asignamos el parámetro pasado al programa a la variable name.
+        String name = args[0];
+        // Mostramos un mensaje en consola con un tiempo de espera aleatorio entre
+        // 1 y 5 segundos para iteración del ciclo for.
+        for (int i = 1; i <= 15; i++) {
+            System.out.println("Hello, I am " + name + " and I am printing the number " + i);
             try {
-                int randomSleepTime = (int) (Math.random() * 4000) + 1000; // Random sleep between 1 and 5 seconds
+                // Genera el tiempo de espera aleatorio
+                int randomSleepTime = (int) (Math.random() * 4000) + 1000;
                 Thread.sleep(randomSleepTime);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
     }
 }
